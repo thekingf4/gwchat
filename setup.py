c56d2211f1b27befa89387628f863505514cef18
Luis Capote <lcapote@opensistemas.com>
2023-04-18 13:55:09 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+import setuptools
+
+setuptools.setup(
+    name="gwchat",
+    version="0.1.",
+    license="MIT",
+    description="gwchat Open edX course_tab",
+    # url="https://github.com/tony-h/rocketchat-tab",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Platform :: Open edX",
+        "Natural Language :: English",
+        "Environment :: Web Environment",
+    ],
+    entry_points={
+        "openedx.course_tab": [
+            "gwchat = gwchat.plugins:GraspwayChatTab",
+        ]
+    },
+)
