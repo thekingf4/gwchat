3f0ecb05ca654f676013b0da85d95f0de34b16cd
Luis Capote <lcapote@opensistemas.com>
2023-04-19 07:58:53 -0500

    error fixed

--- gwchat_tab/urls.py
+++ gwchat_tab/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from django.conf import settings
 from .views import GwChatView
 
-urlpatterns += (
+urlpatterns = (
     url(
         r'^courses/{}/gwchat/'.format(
             settings.COURSE_ID_PATTERN,
